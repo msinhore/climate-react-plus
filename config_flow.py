295c1d31bc9afbf44dec98591221ac7bb8a1f66c
Marco Sinhoreli <msinhore@gmail.com>
2025-06-05 15:17:11 +0200

    Added config_flow.py

---
+++ config_flow.py
@@ -0,0 +1,29 @@
+import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.core import callback
+from homeassistant.const import CONF_NAME, CONF_ENTITY_ID
+from .const import DOMAIN
+
+STEP_USER_DATA_SCHEMA = vol.Schema({
+    vol.Required(CONF_NAME): str,
+    vol.Required("climate_entity"): str,
+    vol.Required("temperature_sensor"): str,
+    vol.Required("enabled_entity"): str,
+    vol.Required("min_temp_entity"): str,
+    vol.Required("max_temp_entity"): str,
+    vol.Required("setpoint_entity"): str,
+    vol.Optional("mode_entity"): str,
+    vol.Optional("fan_entity"): str,
+})
+
+class ClimateReactPlusConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=STEP_USER_DATA_SCHEMA
+        )
