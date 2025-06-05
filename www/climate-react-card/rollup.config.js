c05db4967fb3d54116812715f0adb459fc9a0f38
Marco Sinhoreli <msinhore@gmail.com>
2025-06-05 15:02:16 +0200

    Initial commit with integration, frontend card and build config

--- www/climate-react-card/rollup.config.js
+++ www/climate-react-card/rollup.config.js
@@ -2,7 +2,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 export default {
   input: 'climate-react-card.ts',
