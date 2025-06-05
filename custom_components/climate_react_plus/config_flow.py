8f192b80ebf65cd5e226cb70722134047d816917
Marco Sinhoreli <msinhore@gmail.com>
2025-06-05 15:39:40 +0200

    Added config_flow.py

--- custom_components/climate_react_plus/config_flow.py
+++ custom_components/climate_react_plus/config_flow.py
@@ -1,22 +1,22 @@
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
-from homeassistant.const import CONF_NAME, CONF_ENTITY_ID
+from homeassistant.const import CONF_NAME
+from homeassistant.helpers.selector import selector
 from .const import DOMAIN
 
 STEP_USER_DATA_SCHEMA = vol.Schema({
     vol.Required(CONF_NAME): str,
-    vol.Required("climate_entity"): str,
-    vol.Required("temperature_sensor"): str,
-    vol.Required("enabled_entity"): str,
-    vol.Required("min_temp_entity"): str,
-    vol.Required("max_temp_entity"): str,
-    vol.Required("setpoint_entity"): str,
-    vol.Optional("mode_entity"): str,
-    vol.Optional("fan_entity"): str,
+    vol.Required("climate_entity"): selector({"entity": {"domain": "climate"}}),
+    vol.Required("temperature_sensor"): selector({"entity": {"domain": "sensor"}}),
+    vol.Required("enabled_entity"): selector({"entity": {"domain": "input_boolean"}}),
+    vol.Required("min_temp_entity"): selector({"entity": {"domain": "input_number"}}),
+    vol.Required("max_temp_entity"): selector({"entity": {"domain": "input_number"}}),
+    vol.Required("setpoint_entity"): selector({"entity": {"domain": "input_number"}}),
+    vol.Optional("mode_entity"): selector({"entity": {"domain": "input_select"}}),
+    vol.Optional("fan_entity"): selector({"entity": {"domain": "input_select"}}),
 })
 
-@config_entries.HANDLERS.register(DOMAIN)
 class ClimateReactPlusConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
